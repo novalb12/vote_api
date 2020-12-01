4f0e73245c63950d2b9517705f23ac86002d5940
novalb12 <novalb65@gmail.com>
2020-12-01 14:53:10 +0700

    s

--- database/migrations/2020_11_10_105609_create_calons_table.php
+++ database/migrations/2020_11_10_105609_create_calons_table.php
@@ -21,6 +21,7 @@ class CreateCalonsTable extends Migration
             $table->longText('visi');
             $table->longText('misi');
             $table->longText('proker');
+            $table->string('fakultas');
             $table->integer('suara')->default(0);
             $table->timestamps();
         });
